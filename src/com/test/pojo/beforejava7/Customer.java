6cb20ecfbaaa1e8facadecf81a4e6e08a312422e
Rahamathullah <rahamath18@yahoo.com>
2017-08-27 01:55:04 +0530

    code modified
    code modified

--- src/com/test/pojo/beforejava7/Customer.java
+++ src/com/test/pojo/beforejava7/Customer.java
@@ -1,8 +1,14 @@
 package com.test.pojo.beforejava7;
 
-public class Customer {
+import java.io.Serializable;
+import java.util.Comparator;
+import java.util.Objects;
 
-    int id;
+public class Customer implements Serializable, Comparable<Customer> {
+
+	private static final long serialVersionUID = 1L;
+	
+	int id;
     String name;
     String email;
 
@@ -38,23 +44,85 @@ public class Customer {
 
     @Override
     public boolean equals(Object obj) {
-    	return super.equals(obj);
+    	
+    	Customer cust = (Customer) obj;
+    	
+    	// before java 7
+    	if(cust.getId() == this.id && 
+    			cust.getName().equals(this.name) && 
+    			cust.getEmail().equals(this.email))
+    		return true;
+    	else
+    		return false;
+    	
+    	// from java 7
+//    	return cust.getId() == this.id &&
+//    			Objects.equals(name, cust.name) &&
+//    			Objects.equals(email, cust.email);
     }
     
     @Override
     public int hashCode() {
-    	return super.hashCode();
-    	//public int hashCode() {
-        //return hash ^ Objects.hashCode(value);
-    	//}
+    	
+    	// before java 7
+        int result = 17;
+        result = 31 * result + id;
+        result = 31 * result + name.hashCode();
+        result = 31 * result + email.hashCode();
+        return result;
+        
+        // from java 7
+        // return Objects.hash(id, name, email);
     }
     
     @Override
     public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
+    	return id +"|"+ name +"|"+ email;
     }
+    
+    /*
+     * This can be used while sorting the collection of customers objects 
+     * with passing Comparator ref to Collections.sort(aColl)
+     */
+    @Override
+	public int compareTo(Customer e) {
+    	
+    	final int BEFORE = -1;
+        final int EQUAL = 0;
+        final int AFTER = 1;
+        
+        if (this == e) return EQUAL;
+        
+		if (this.id > e.id) 
+			return AFTER;
+		
+		if (this.id < e.id) 
+			return BEFORE;
+		
+		return EQUAL;
+	}
+    
+    /*
+     * This can be used while sorting the collection of customers objects 
+     * with passing Comparator ref to Collections.sort(aColl, Comparator)
+     */
+    public static Comparator<Customer> idComparator = new Comparator<Customer>() {
+ 		@Override
+ 		public int compare(Customer c1, Customer c2) {
+ 			return Integer.valueOf(c1.getId()).compareTo(Integer.valueOf(c2.getId()));
+ 		}
+ 	};
+    
+ 	/*
+     * This can be used while sorting the collection of customers objects 
+     * with passing Comparator ref to Collections.sort(aColl, Comparator)
+     */
+ 	public static Comparator<Customer> nameComparator = new Comparator<Customer>() {
+ 		@Override
+ 		public int compare(Customer c1, Customer c2) {
+ 			return c1.getName().compareTo(c2.getName());
+ 		}
+ 	};
+ 	
+ 	// you may add as many Comparator and all about how you want sort or order your POJO
 }
---
+++ src/com/test/pojo/beforejava7/TestCustomerPOJO.java
@@ -0,0 +1,22 @@
+package com.test.pojo.beforejava7;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestCustomerPOJO {
+	
+	public static void main(String[] args) {
+		
+		Customer cust1 = new Customer(1, "rahamath", "rahamath18@yahoo.com");
+		Customer cust2 = new Customer(1, "rahamath", "rahamath18@yahoo.com");
+		
+		System.out.println(cust1.equals(cust2));
+		
+		Map<Customer, String> aMap = new HashMap<Customer, String>();
+		aMap.put(cust1, "1");
+		aMap.put(cust2, "2");
+		System.out.println(aMap);
+
+	}
+
+}
